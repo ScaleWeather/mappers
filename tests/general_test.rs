5fd0dff5adb5f829bb1cdb15fc39da5796472690
Quba1 <jak.lewand@gmail.com>
2022-06-28 12:01:47 +0200

    bugfix

--- tests/general_test.rs
+++ tests/general_test.rs
@@ -1,8 +1,8 @@
-use mappers::{LambertConicConformal, Projection};
+use mappers::{LambertConicConformal, Projection, constants::WGS84};
 
 #[test]
 fn test_projection() {
-    let lcc = LambertConicConformal::new(18.0, 0.0, 30.0, 60.0).unwrap();
+    let lcc = LambertConicConformal::new(18.0, 0.0, 30.0, 60.0, WGS84).unwrap();
 
     let (ref_lon, ref_lat) = (18.58973722443749, 54.41412855026378);
 
