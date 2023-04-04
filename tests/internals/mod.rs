2fee22e2764cc711a13dd9d7195679e9f5857625
Quba1 <22771850+Quba1@users.noreply.github.com>
2023-04-04 12:45:49 +0200

    fix - debug in tests in production

--- tests/internals/mod.rs
+++ tests/internals/mod.rs
@@ -50,7 +50,6 @@ static MAP_POINTS: [(f64, f64); 8] = [
     (-200_000.0, -200_000.0),
 ];
 
-#[allow(unused)]
 pub fn test_points_with_proj(int_proj: &dyn Projection, proj_str: &str, extent: TestExtent) {
     let ref_proj = Proj::new(proj_str).unwrap();
 
@@ -66,12 +65,8 @@ pub fn test_points_with_proj(int_proj: &dyn Projection, proj_str: &str, extent: 
 
         let (tst_x, tst_y) = int_proj.project(point.0, point.1).unwrap();
 
-        //assert_approx_eq!(f64, ref_x, tst_x, epsilon = 0.000_000_1);
-        //assert_approx_eq!(f64, ref_y, tst_y, epsilon = 0.000_000_1);
-        println!(
-            "{:?}\nref x:{:.6} y:{:.6}\ntst x:{:.6} y:{:.6}\n",
-            point, ref_x, ref_y, tst_x, tst_y
-        );
+        assert_approx_eq!(f64, ref_x, tst_x, epsilon = 0.000_000_1);
+        assert_approx_eq!(f64, ref_y, tst_y, epsilon = 0.000_000_1);
     }
 
     for point in MAP_POINTS {
